5607d18d8e04ecbe6404615f1462ad4a7d90b3bd
Alan Spires <alan.spires@gmail.com>
2017-06-08 15:53:03 -0600

    added a couple GIF's to listview example for debugging

--- samples/ImageLoading.Forms.Sample/Shared/Pages/ListPageModel.cs
+++ samples/ImageLoading.Forms.Sample/Shared/Pages/ListPageModel.cs
@@ -29,9 +29,9 @@ namespace FFImageLoading.Forms.Sample
 			var list = new List<ListItem>();
 
 			string[] images = {
-				"https://farm9.staticflickr.com/8625/15806486058_7005d77438.jpg",
+				"https://media.giphy.com/media/3o6wre4HS7nzNWbt8Q/giphy.gif",
 				"https://farm5.staticflickr.com/4011/4308181244_5ac3f8239b.jpg",
-				"https://farm8.staticflickr.com/7423/8729135907_79599de8d8.jpg",
+				"https://media.giphy.com/media/l0HlPtbGpcnqa0fja/giphy.gif",
 				"https://farm3.staticflickr.com/2475/4058009019_ecf305f546.jpg",
 				"https://farm6.staticflickr.com/5117/14045101350_113edbe20b.jpg",
 				"https://farm2.staticflickr.com/1227/1116750115_b66dc3830e.jpg",
